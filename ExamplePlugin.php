ce5c3f4363c18f0478845c9a478d5e4207463447
PEMapModder <pemapmodder1970@gmail.com>
2014-04-07 17:06:32 +0800

    Rewrite

--- ExamplePlugin.php
+++ ExamplePlugin.php
@@ -1,5 +1,7 @@
 <?php
 
+// description
+
 /*
 __PocketMine Plugin__
 class=AnOldApiPlugin
@@ -9,20 +11,28 @@ version=1.0
 apiversion=12
 */
 
+// information
+
 // no namespaces needed
 
 class AnOldApiPlugin implements Plugin{
+	// start
 	public function __construct(ServerAPI $api, $server = false){
 		$this->api = $api;
 	}
+	// initialize
 	public function init(){
 		$this->api->addHandler("player.block.touch", array($this, "eventHandler"), 10);
 		$this->api->console->register("echo", "echos a command to console", array($this, "commandHandler"));
+		$this->api->console->alias("ec", "echo");
+		$this->api->console->alias("eh", "echo");
 		$this->api->schedule(1200, array($this, "delayedFunction"), array("item 0", "item 1"), false);
 		$this->api->schedule(700, array($this, "repeatedFunction"), array("item 2", "item 3"), true);
 	}
+	// end
 	public function __destruct(){
 	}
+	// handle commands
 	public function commandHandler($cmd, $params, $issuer, $alias){
 		switch($cmd){
 			case "echo":
@@ -30,6 +40,7 @@ class AnOldApiPlugin implements Plugin{
 				return "You have used /$cmd.";
 		}
 	}
+	// handle events
 	public function eventHandler($data, $event){
 		switch($event){
 			case "player.block.touch":
@@ -38,9 +49,11 @@ class AnOldApiPlugin implements Plugin{
 				break;
 		}
 	}
+	// delayed schedule
 	public function delayedFunction($data){
 		var_dump($data);
 	}
+	// repeated schedule
 	public function repeatedFunction($data){
 		var_dump($data);
 	}
